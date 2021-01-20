f99f473ad8d978e46f41a3597e2546f09bce42ed
soshimozi <scott.mccain@gmail.com>
2021-01-20 13:26:04 -0800

    Some refactors for dice roll

--- src/commands/RollDiceCommand.ts
+++ src/commands/RollDiceCommand.ts
@@ -4,8 +4,8 @@ import crypto from "crypto";
 import DiceRollEvaluator from "../lib/DiceRoleEvaluator";
 
 export const rolldice:ICommand =  {
-    name: 'rolldice',
-    usage: `${process.env.PREFIX}rolldice dice-formula.  See help for some examples.`,
+    name: 'roll',
+    usage: `${process.env.PREFIX}roll dice-formula.  See help for some examples.`,
 
     handler: async (client: Client, message:Message, ...parameters: string[]):Promise<void> => {
         if(parameters.length < 1) {
@@ -22,21 +22,7 @@ export const rolldice:ICommand =  {
         catch(err) {
             await message.reply(`The formula you supplied is invalid.  Please supply a valid formula.  See help for some examples.`)
         }
-        // const dieNumber = parseInt(parameters[0].trim());
-        // if(isNaN(dieNumber)) {
-        //     await message.reply(rolldice.usage);
-        //     return;
-        // }
 
-        // const validDice = [2, 4, 6, 8, 10, 12, 20, 100]
-        // if(validDice.indexOf(dieNumber) === -1) {
-        //     await message.reply(rolldice.usage);
-        //     return;
-        // }
-
-        // const result = crypto.randomInt(1, dieNumber+1);
-
-        //await message.channel.send(`${message.author.username} just rolled a ${result} on a 1d${dieNumber} `);
         return;
 
     }
