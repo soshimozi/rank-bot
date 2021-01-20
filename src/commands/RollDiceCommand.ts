e49e2c45c1da70434f8a0397a0079749d34a7157
soshimozi <scott.mccain@gmail.com>
2021-01-20 10:30:55 -0800

    Fixed issue with rolldice allowing other than specified dice

--- src/commands/RollDiceCommand.ts
+++ src/commands/RollDiceCommand.ts
@@ -4,7 +4,7 @@ import crypto from "crypto";
 
 export const rolldice:ICommand =  {
     name: 'rolldice',
-    usage: `${process.env.PREFIX}rolldice {die number}, where die number is in [2,4,6,10,12,20,100]`,
+    usage: `${process.env.PREFIX}rolldice {die number}, where die number is in [2,4,6,8,10,12,20,100]`,
 
     handler: async (client: Client, message:Message, ...parameters: string[]):Promise<void> => {
         if(parameters.length < 1) {
@@ -14,6 +14,12 @@ export const rolldice:ICommand =  {
 
         const dieNumber = parseInt(parameters[0]);
         if(isNaN(dieNumber)) {
+            await message.reply(rolldice.usage);
+            return;
+        }
+
+        const validDice = [2, 4, 6, 8, 10, 12, 20, 100]
+        if(validDice.indexOf(dieNumber) === -1) {
             await message.reply(rolldice.usage);
             return;
         }
