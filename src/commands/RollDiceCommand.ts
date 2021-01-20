de81df65ddbd9fe5e982574dfc45f4965d3fca1a
soshimozi <scott.mccain@gmail.com>
2021-01-20 12:36:35 -0800

    Added dice roll evaluator

--- src/commands/RollDiceCommand.ts
+++ src/commands/RollDiceCommand.ts
@@ -1,10 +1,12 @@
 import { Client, Message } from "discord.js";
 import { ICommand } from "../interfaces/ICommand";
 import crypto from "crypto";
+import { try } from "bluebird";
+import DiceRollEvaluator from "../lib/DiceRoleEvaluator";
 
 export const rolldice:ICommand =  {
     name: 'rolldice',
-    usage: `${process.env.PREFIX}rolldice {die number}, where die number is in [2,4,6,8,10,12,20,100]`,
+    usage: `${process.env.PREFIX}rolldice dice-formula.  See help for some examples.`,
 
     handler: async (client: Client, message:Message, ...parameters: string[]):Promise<void> => {
         if(parameters.length < 1) {
@@ -12,21 +14,30 @@ export const rolldice:ICommand =  {
             return;
         }
 
-        const dieNumber = parseInt(parameters[0]);
-        if(isNaN(dieNumber)) {
-            await message.reply(rolldice.usage);
-            return;
+        let value:number;
+
+        try {
+            value = DiceRollEvaluator.evaluateRoll(parameters[0]);
+            await message.channel.send(`${message.author.username} just rolled a ${value} with function ${parameters[0]} `);
         }
-
-        const validDice = [2, 4, 6, 8, 10, 12, 20, 100]
-        if(validDice.indexOf(dieNumber) === -1) {
-            await message.reply(rolldice.usage);
-            return;
+        catch(err) {
+            await message.reply(`The formula you supplied is invalid.  Please supply a valid formula.  See help for some examples.`)
         }
+        // const dieNumber = parseInt(parameters[0].trim());
+        // if(isNaN(dieNumber)) {
+        //     await message.reply(rolldice.usage);
+        //     return;
+        // }
 
-        const result = crypto.randomInt(1, dieNumber);
+        // const validDice = [2, 4, 6, 8, 10, 12, 20, 100]
+        // if(validDice.indexOf(dieNumber) === -1) {
+        //     await message.reply(rolldice.usage);
+        //     return;
+        // }
 
-        await message.channel.send(`${message.author.username} just rolled a ${result} on a 1d${dieNumber} `);
+        // const result = crypto.randomInt(1, dieNumber+1);
+
+        //await message.channel.send(`${message.author.username} just rolled a ${result} on a 1d${dieNumber} `);
         return;
 
     }
